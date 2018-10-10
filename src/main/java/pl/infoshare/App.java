cdf408c1132e1263e88b1d35c5e7137cba383888
Jakub Nadzieja <nadziej1@o2.pl>
2018-10-10 17:31:02 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,19 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int suma = 10+5;
+        int roznica = 10-5;
+        int iloczyn=10*5;
+        int iloraz=10/2;
+
+        System.out.println(suma, roznica, iloczyn, iloraz);
+
+    }
+}
