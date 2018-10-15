6f4d6b0a4ef726368d26f9eab095f842fb3ec141
Jakub Nadzieja <nadziej1@o2.pl>
2018-10-15 19:03:08 +0200

    added 15.10

--- src/main/java/pl/infoshare/App.java
+++ src/main/java/pl/infoshare/App.java
@@ -1,19 +1,45 @@
 package pl.infoshare;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        int suma = 10+5;
-        int roznica = 10-5;
-        int iloczyn=10*5;
-        int iloraz=10/2;
+public class App {
+    public static void main(String[] args) {
 
-        System.out.println(suma, roznica, iloczyn, iloraz);
+//        int []arrayFor=new.int[1000];
+//
+//        for (int i =0;i<1000;i++)
+//        {
+//            arrayFor[i]=i+1;
+//        }
+//
+//        for(int element: arrayFor) {
+//            System.out.println(element);
+//        }
 
+//        PETLA 2D
+//
+//
+//        int[][] array2D=new int[10][10];
+//
+//        for(int i=0 ; i<array2D.length ; i++){
+//            System.out.println("I'm in row no. "+i);
+//            for (int j=0 ; j<array2D.length; j++){
+//                System.out.println("I'm in a column no. "+j);
+//            
+//
+
+        int[][] array2D = new int[10][10];
+
+        for (int i = 1; i < array2D.length + 1; i++) {
+            System.out.print(i + ". ");
+            for (int j = 1; j < array2D.length + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
